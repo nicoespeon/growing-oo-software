3a47369b9001cc6d44ffab1fb6c04dfa9bb7f24e
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-08-05 21:49:38 +0200

    Fake basic XMPP connection features

--- src/lib/xmpp/connection.ts
+++ src/lib/xmpp/connection.ts
@@ -1,17 +1,51 @@
 export { Connection, Chat, Message, MessageListener };
 
+// Store connections in memory so we can simulate they connect to the same hostname
+const connections = new Map<string, Connection>();
+
 class Connection {
-  constructor(hostName: string) {}
-  connect() {}
-  login(login: string, password: string, resource: string) {}
-  getChatManager(): ChatManager {
-    return new ChatManager();
+  private chatManager: ChatManager | null = null;
+
+  constructor(private hostName: string) {
+    // TODO: This should be built with connect() and login() instead
+    const existingConnection = connections.get(hostName);
+    if (existingConnection) {
+      return existingConnection;
+    }
+
+    connections.set(hostName, this);
   }
+
+  get serviceName(): string {
+    return this.hostName;
+  }
+
+  connect() {}
+
+  login(login: string, password: string, resource: string) {}
+
+  getChatManager(): ChatManager {
+    if (!this.chatManager) {
+      this.chatManager = new ChatManager();
+    }
+
+    return this.chatManager;
+  }
+
   disconnect() {}
 }
 
 class ChatManager {
-  addChatListener(listener: ChatListener) {}
+  private chat = new Chat();
+
+  addChatListener(listener: ChatListener) {
+    listener.chatCreated(this.chat, true);
+  }
+
+  createChat(id: string, listener: MessageListener): Chat {
+    this.chat.addMessageListener(listener);
+    return this.chat;
+  }
 }
 
 interface ChatListener {
@@ -19,13 +53,21 @@ interface ChatListener {
 }
 
 class Chat {
-  addMessageListener(listener: MessageListener) {}
-  sendMessage(message: Message) {}
+  private listeners: MessageListener[] = [];
+
+  addMessageListener(listener: MessageListener) {
+    this.listeners.push(listener);
+  }
+
+  sendMessage(message: Message) {
+    this.listeners.forEach((listener) =>
+      listener.processMessage(this, message)
+    );
+  }
 }
 
 class Message {}
 
 interface MessageListener {
   processMessage: (chat: Chat, message: Message) => void;
-  receivesAMessage: () => void;
 }
